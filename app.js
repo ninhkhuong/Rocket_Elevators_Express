9c8e912083e914ea7991666e55d8f1805e505642
Ninh K <ninh.bkhuong@gmail.com>
2023-02-27 22:03:38 -0500

    route returns error

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 8080;
 const environment = process.env.ENVIRONMENT || "development";
 
 
+//Create a route that returns an error code and message
+app.get('/error', (req,res) => {
+  res.status(500).send('Internal Server Error');
+});
 
 //Create a route that returns a message containing the port and environment name from the .env file
 app.get('/', (req,res)=>{
