54d221ee43687c361d810a069e9a21399cd976f6
Ninh K <ninh.bkhuong@gmail.com>
2023-02-27 21:54:11 -0500

    added .env

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.listen(port, () => {
-  console.log(` server listening on port ${port} `)
-})
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+const environment = process.env.ENVIRONMENT || "development";
+
+
+
+//Create a route that returns a message containing the port and environment name from the .env file
+app.get('/', (req,res)=>{
+  res.send(`Sever is running on port ${port} in ${environment} mode`);
+});
+
+
+app.listen(port,() =>{
+  console.log(`Server is listening on ${port}`);
+});
+
+
