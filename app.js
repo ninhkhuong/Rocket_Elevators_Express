0e24c8ccc4dfee2fd3c65a9bfea9e561a9fe1758
Ninh K <ninh.bkhuong@gmail.com>
2023-03-01 15:55:01 -0500

    region query for agents.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const environment = process.env.ENVIRONMENT;
 // agents.js
 const agents = require('./agents');
 
+//Create a route that returns list of emails in agents.js
 app.get('/agents',(req,res) => {
   const results = [];
 
@@ -18,6 +19,42 @@ app.get('/agents',(req,res) => {
 
   res.send(results);
   });
+
+
+
+//route that accepts a region as a query parameter, get average rating and fee for agents in a specific region
+//http://localhost:3000/region-avg?region=east
+
+app.get('/region-avg', (req, res) => {
+  const region = req.query.region;
+  const regionAgents = [];
+  
+  agents.forEach(agent => {
+    if (agent.region === region) {
+      regionAgents.push(agent);
+    }
+  });
+ 
+  if (regionAgents.length === 0) {
+    res.status(404).send(`No agents found in region: ${region}`);
+    return;
+  }
+
+  const ratingSum = regionAgents.reduce((sum, agent) => sum + Number(agent.rating), 0);
+  const feeSum = regionAgents.reduce((sum, agent) => sum + Number(agent.fee), 0);
+  const avgRating = ratingSum / regionAgents.length;
+  const avgFee = feeSum / regionAgents.length;
+
+  const response = {
+    region: region,
+    averageRating: avgRating.toFixed(2),
+    averageFee: avgFee.toFixed(2)
+  };
+  res.json(response);
+});
+
+
+  
 
 
 //Create a route that returns an error code and message
